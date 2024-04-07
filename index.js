688a5fcb65514cb7fea15e8ff1de5bfa6ba7d54a
Patrick Herbst <p.herbst.1987@gmail.com>
2024-04-07 09:25:41 +0200

    Changed database information to environmental variable

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import env from "dotenv";
 
 const app = express();
 const port = 3000;
+env.config();
 
 // postgreSQL DB
 const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "blog",
-    password: "atomicFluencers!",
-    port: 5432,
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
   });
   db.connect();
 
